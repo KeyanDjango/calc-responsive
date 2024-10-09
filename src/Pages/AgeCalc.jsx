d70a9044d49eea5b8ca3438807f0bf45ae9583f9
KeyanDjango <karthick.mendix@gmail.com>
2024-10-09 17:17:44 +0530

    changes in age calc

--- src/Pages/AgeCalc.jsx
+++ src/Pages/AgeCalc.jsx
@@ -3,18 +3,21 @@ import Styles from './AgeCalc.module.css';
 
 export default function AgeCalc() {
     const [datecalc, setDateCalc] = useState('');
-    const[result_age,setResultAge] = useState(0);
+    const [result_age, setResultAge] = useState(0);
     const current_date = new Date();
 
-    function handleClick(){
+    function handleClick() {
         if (datecalc) {
             const dob_date = new Date(datecalc);
-            let result =current_date.getFullYear() - dob_date.getFullYear();
+            let result = current_date.getFullYear() - dob_date.getFullYear();
             setResultAge(result);
         }
     }
-    
 
+    function handleClear(){
+        setDateCalc('');
+        setResultAge(0)
+    }
     return (
         <div>
             <h1 style={{ textAlign: 'center' }}>AGE CALCULATOR</h1>
@@ -22,7 +25,12 @@ export default function AgeCalc() {
             <div className={Styles.agecalc_parent_con}>
                 <div className={Styles.agecalc_child_con}>
                     <input value={datecalc} type='date' onChange={(e) => setDateCalc(e.target.value)} />
-                    <button className={Styles.agebtn} onClick={handleClick}>CALCULATE</button>
+
+                    <div style={{display:'flex'}}>
+                        <button className={Styles.agebtn} onClick={handleClick}>CALCULATE</button>
+                        <button className={Styles.agebtn}  onClick={handleClear}>CLEAR</button>
+                    </div>
+
                     <h4 style={{ marginTop: 20 }}>{result_age} <span style={{ color: 'blue', fontWeight: 'bold' }}> YEARS OLD </span></h4>
                 </div>
             </div>
